cc794a177d937788e1880aea8f8d59baf8372a9d
Jamshid Toshov <jamik.oik.group@gmail.com>
2024-12-11 21:07:40 +0500

    icon and title added

--- app.vue
+++ app.vue
@@ -4,3 +4,16 @@
     <NuxtPage />
   </div>
 </template>
+
+<script>
+useHead({
+  title: 'Ask you out?',
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/icon512_rounded.png'
+    }
+  ]
+})
+</script>
